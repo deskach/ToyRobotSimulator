ad0b6ee81a600e88f7ecbf86ff5466c9ca742f40
deskach <dzianis.kachanau@gmail.com>
2015-11-20 07:16:30 +1100

    added a placeholder for stateReady Unit Tests

---
+++ tests/stateReadyUT.js
@@ -0,0 +1,53 @@
+﻿var assert = require('assert');
+
+var StateReady = require('../stateReady').StateReady;
+var sConsts = require('../constants').stateConstants;
+var snReady = require('../constants').stateMachineConstants.stateNames.READY;
+var snUndefined = require('../constants').stateMachineConstants.stateNames.UNDEFINED;
+
+describe('StateReady', function () {
+  var stateReady;
+
+  function initVars() {
+    states = {};
+    states[snReady] = stateReady;
+    states[snUndefined] = {};
+    
+    stateMachineMoq = {
+      states: states
+    };
+    
+    stateReady = new StateReady({
+      stateMachine: stateMachineMoq
+    });
+  };
+  
+  describe('.next() accepts', function () {
+    beforeEach(function () { initVars(); });
+    
+    it('MOVE command');
+    
+    it('REPORT command');
+    
+    it('LEFT command');
+    
+    it('RIGHT command');
+    
+    it('PLACE command');
+  });
+
+  describe('.run() processes', function () {
+    beforeEach(function () { initVars(); });
+    
+    it('MOVE command');
+    
+    it('REPORT command');
+    
+    it('LEFT command');
+    
+    it('RIGHT command');
+    
+    it('PLACE command');
+  })
+
+})
