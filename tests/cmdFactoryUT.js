281b8137c33e4b942553457aa889abe3a7bee4be
deskach <dzianis.kachanau@gmail.com>
2015-11-20 14:15:26 +1100

    added a stub for CommandFactory Unit Tests

---
+++ tests/cmdFactoryUT.js
@@ -0,0 +1,15 @@
+﻿var assert = require('assert');
+
+describe('CommandFactory', function () {
+  it('provides default constructors');
+
+  describe('default constructors support commands', function () {
+    it('PLACE');
+    it('LEFT');
+    it('RIGHT');
+    it('REPORT');
+    it('MOVE');
+  });
+
+  it('can be configured to use custom constructors');
+})
