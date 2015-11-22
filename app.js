ffb6daa69129be53330836b1bbc2ffeea70949f9
deskach <dzianis.kachanau@gmail.com>
2015-11-23 10:04:02 +1100

    Replaced process.stdIn by 'readline' module, because it allows for asynchronous execution.

--- app.js
+++ app.js
@@ -2,28 +2,43 @@
 var StateReady = require('./stateReady').StateReady;
 var StateMachine = require('./stateMachine').StateMachine;
 var CommandFactory = require('./cmdFactory.js').CommandFactory;
-var cmdConsts = require('./constants').commandConstants;
 
 var states = [new StateUndefined(), new StateReady()];
 var stateMachine = new StateMachine(states, StateUndefined.prototype.name());
 var cmdFactory = new CommandFactory();
-var stdin = process.openStdin();
+var readline = require('readline');
+
 var input = [];
 
-console.log('Write commands and hit <ENTER> to execute all of them')
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
 
-stdin.addListener("data", function (d) {
-  var strCmd = d.toString().trim();
+console.log('Write commands one command per line.');
+console.log('Empty command will trigger robot to execute the queue of commands.');
 
+rl.setPrompt('CMD>');
+rl.prompt();
+
+rl.on('line', function (d) {
+  var strCmd = d.trim();
+  
   if (strCmd.length > 0) {
     try {
       var cmd = cmdFactory.create(strCmd);
       input.push(cmd);
     } catch (e) {
       console.log('Wrong command "' + strCmd + '"');
-    }        
+    }
   } else {
     stateMachine.runAll(input);
     input = [];
   }
+  
+  rl.prompt();
+}).on('close', function () {
+  console.log('See you!');
+  process.exit(0);
 });
